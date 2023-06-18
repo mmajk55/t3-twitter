2fd7f87b85cfe5c4eb79e8468c4045407abd4a8f
mmajk55 <michalmalec55@gmail.com>
2023-06-18 21:43:17 +0200

    handle loading states

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -63,7 +63,7 @@ const PostsView = (props: PostWithAuthor) => {
               post.createdAt
             ).fromNow()}`}</span>
           </div>
-          <span>{post.content}</span>
+          <span className="text-2xl">{post.content}</span>
         </div>
       </div>
     </div>
